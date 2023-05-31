7f5d0ecd35f47e5de683bc994a7d7bdfa0dc9589
rajk5266 <kushwaharaj903@gmail.com>
2023-05-31 14:42:47 +0530

    async await

--- main.js
+++ main.js
@@ -1,148 +1,93 @@
-    window.addEventListener('DOMContentLoaded',() =>{
-    axios.get('https://crudcrud.com/api/fae1b38a234e47e7882ebb3676ff7a74/productdetails')
-    .then((response) =>{
-        for(let i=0; i<response.data.length; i++){
-            reloaddata(response.data[i])   
-        }
-        var li = document.querySelectorAll('.newlist')
-        let x = document.querySelectorAll('.sellbtn')
-        let z = document.querySelectorAll('.sellinput')
-        // console.log(x)
-        for(let i=0; i<li.length; i++){
+window.addEventListener('DOMContentLoaded', async () => {
+    try {
+      let response = await axios.get('https://crudcrud.com/api/7cfd2de103a54e2986d2e35382b9352c/productdetails')
+  
+      for (let i = 0; i < response.data.length; i++) {
+        reloaddata(response.data[i])
+      }
+  
+      let li = document.querySelectorAll('.newlist')
+      let x = document.querySelectorAll('.sellbtn')
+      let z = document.querySelectorAll('.sellinput')
+  
+      for (let i = 0; i < li.length; i++) {
+        x[i].onclick =  () => {
+          let inputvalue = z[i].value
+  
+          if (response.data[i].quantity == 0) {
+            alert('The product is out of stock')
+            return;
+          } else if (response.data[i].quantity < inputvalue) {
+            alert('Selling quantity should be less than available quantity')
+            return
+          }
+           else {
+            alert('confirm ?')
+          }
+  
+          const updatedquantity = parseInt(response.data[i].quantity - inputvalue)
+
+          li[i].textContent = '[ Product=' + response.data[i].product + '] [Description=' + response.data[i].description + '] [Price=' + response.data[i].price + '] [Quantity=' + updatedquantity + ']'
+  
+            updateProduct(response.data[i]._id, {
+              product: response.data[i].product,
+              description: response.data[i].description,
+              price: response.data[i].price,
+              quantity: updatedquantity
+            })
             
-            x[i].onclick =() =>{
-                
-                let inputvalue = parseInt(z[i].value)
-            //    if(inputvalue.length == 0)
-            //    {alert('wrong');
-            //       return false
-            //       }
-                if(response.data[i].quantity == 0){
-                    // console.log("error")
-                    alert('out of stock')
-                   return 
-                }
-                else if(response.data[i].quantity < inputvalue){
-                    alert('Input number should be less than available quantity ')
-                    return 
-                }
-                else{
-                    alert('confirm ?')
-                }
-                // console.log(li[i])
-                // console.log(z[i].value)
-                // console.log(inputvalue)
-                // console.log(i)
-                // console.log('btn 1 is clicked')
-               li[i].textContent = '[ Product=' + response.data[i].product + '] [Description=' + response.data[i].description  +'] [Price='+ response.data[i].price + '] [Quantity=' + parseInt(response.data[i].quantity-inputvalue)+ ']'
-               
-     const sellbtn = document.createElement('input')
-     const sellinput = document.createElement('input')
-     sellbtn.className='sellbtn'
-     sellinput.className='sellinput'
-     sellbtn.type = 'button'
-     sellinput.type = 'text'
-     sellbtn.value = 'sell-item'
-     sellinput.placeholder = 'Add-quantities to sell'
-     sellinput.required = true
-     li[i].appendChild(sellinput)
-     li[i].appendChild(sellbtn)
-    //  console.log(response.data[i].product)
-
-      axios.put(`https://crudcrud.com/api/fae1b38a234e47e7882ebb3676ff7a74/productdetails/${response.data[i]._id}`,{
-        product : response.data[i].product,
-        description : response.data[i].description,
-        price : response.data[i].price,
-        quantity: response.data[i].quantity-inputvalue
-      })
-      .then((response) => {
-        // console.log(response)
-        location.reload()
-      })
-      .catch((erro) => {
-        console.log('erro')
-      })
-                    }
+            setTimeout(() => {
+                location.reload();
+              }, 500)
+            
         }
-    })
-    .catch((error) => {
-        alert('something went wrong')
-    })
+      }
+    }
+     catch (error) {
+      alert('something went wrong');
+    }
+  })
+
+   function updateProduct(productId, updatedData) {  
+     axios.put(`https://crudcrud.com/api/7cfd2de103a54e2986d2e35382b9352c/productdetails/${productId}`, updatedData)
     
-    // return
-} )
-// location.reload()
+  }
+  
+  
+  function reloaddata(obj) {
 
-function reloaddata(obj){
-    // axios.delete(`https://crudcrud.com/api/895897f35b0e4524973943b2d578c74c/productdetails/${obj._id}`)
-    let parentelem = document.getElementById('lists')
-
-    let childelem = document.createElement('li')
-    childelem.className='newlist'
-     childelem.textContent = '[ Product=' + obj.product + '] [Description=' + obj.description +'] [Price='+ obj.price + '] [Quantity=' + obj.quantity + ']'
-
-     const sellbtn = document.createElement('input')
-     const sellinput = document.createElement('input')
-     sellbtn.className='sellbtn'
-     sellinput.className='sellinput'
-     sellbtn.type = 'button'
-     sellinput.type = 'text'
-     sellbtn.value = 'sell-item'
-     sellinput.placeholder = 'Add-quantities to sell'
-
-     childelem.appendChild(sellinput)
-     childelem.appendChild(sellbtn)
-     parentelem.appendChild(childelem)
-
+    // axios.delete(`https://crudcrud.com/api/7cfd2de103a54e2986d2e35382b9352c/productdetails/${obj._id}`)
+    const parentelem = document.getElementById('lists')
+    const childelem = document.createElement('li')
+    childelem.className = 'newlist'
+    childelem.textContent = '[ Product=' + obj.product + '] [Description=' + obj.description + '] [Price=' + obj.price + '] [Quantity=' + parseInt(obj.quantity) + ']'
+    const sellbtn = document.createElement('input')
+    const sellinput = document.createElement('input')
+    sellbtn.className = 'sellbtn'
+    sellinput.className = 'sellinput'
+    sellbtn.type = 'button'
+    sellinput.type = 'text'
+    sellbtn.value = 'sell-item'
+    sellinput.placeholder = 'Add-quantities to sell'
+    childelem.appendChild(sellinput)
+    childelem.appendChild(sellbtn)
+    parentelem.appendChild(childelem)
+  }
+  
+   function submitting(event) {
+    event.preventDefault();
+    const product = event.target.product.value;
+    const description = event.target.description.value;
+    const price = event.target.price.value;
+    const quantity = parseInt(event.target.quantity.value);
+    const obj = {
+      product,
+      description,
+      price,
+      quantity
     }
-
-function submitting(event){
-    event.preventDefault()
-
-    // var listsbox = document.getElementById("lists")
-    var product = event.target.product.value
-    var description = event.target.description.value
-    var price = event.target.price.value
-    var quantity = parseInt(event.target.quantity.value)
-
-    const obj ={
-        product,
-        description,
-        price,
-        quantity
-    }
-    // console.log(obj)
-    // showlist(obj)
-    axios.post('https://crudcrud.com/api/fae1b38a234e47e7882ebb3676ff7a74/productdetails', obj)
-        .then((response) => {
-            // showlist(response.data)
-            // console.log(response)
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-
-        
-    // function showlist(obj){
-    //     var parentelem = document.getElementById('lists')
-
-    //     var childelem = document.createElement('li')
-    //     childelem.className='newlist'
-    //      childelem.textContent = '[ Product=' + obj.product + '] [Description=' + obj.description +'] [Price='+ obj.price + '] [Quantity=' + obj.quantity + ']'
-
-    //      const sellbtn = document.createElement('input')
-    //      const sellinput = document.createElement('input')
-    //      sellbtn.className='sellbtn'
-    //      sellinput.className='sellinput'
-    //      sellbtn.type = 'button'
-    //      sellinput.type = 'text'
-    //      sellbtn.value = 'sell-item'
-    //      sellinput.placeholder = 'Add-quantity'
-       
-    //      childelem.appendChild(sellinput)
-    //      childelem.appendChild(sellbtn)
-    //      parentelem.appendChild(childelem)
-    
-    //     }
-    location.reload()
-    }
+      axios.post('https://crudcrud.com/api/7cfd2de103a54e2986d2e35382b9352c/productdetails', obj)
+ 
+    location.reload();
+  }
+  
